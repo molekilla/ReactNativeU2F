75673e5dd15a2f84790389a0bb256166492c8b00
Rogelio Morrell <molekilla@gmail.com>
2016-07-15 17:00:44 -0500

    login form

--- src/components/login.js
+++ src/components/login.js
@@ -1,6 +1,13 @@
 import * as React from 'react';
+import * as axios from 'axios';
 import { TouchableHighlight, StyleSheet, Text, TextInput, View } from 'react-native';
 const styles = StyleSheet.create({
+    wrapper: {
+        borderRadius: 8,
+    },
+    button: {
+        color: '#007AFF',
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -13,7 +20,7 @@ const styles = StyleSheet.create({
         margin: 5,
     },
     label: {
-        fontSize: 16,
+        fontSize: 17,
         textAlign: 'center',
         margin: 3,
     },
@@ -24,15 +31,40 @@ const styles = StyleSheet.create({
         height: 50,
         textAlign: 'center',
     },
+    row: {
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    block: {
+        padding: 10,
+    },
+    nativeFeedbackButton: {
+        textAlign: 'center',
+        margin: 10,
+    },
 });
+const API = 'https://app.auth2factor.com/api';
+class LoginReq {
+}
 export class LoginComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.state = new LoginReq();
     }
     onLogin() {
+        console.log(this.state);
+    }
+    login(param) {
+        return axios.post('/v2/users/authenticate', param)
+            .then((response) => {
+            let model = response.data;
+        })
+            .catch((error) => {
+            console.error(error);
+        });
     }
     render() {
-        return (React.createElement(View, {style: styles.container}, React.createElement(Text, {style: styles.title}, "react-native-u2f"), React.createElement(Text, {style: styles.label}, "Username"), React.createElement(TextInput, {style: styles.inputBox}), React.createElement(Text, {style: styles.label}, "Password"), React.createElement(TextInput, {secureTextEntry: true, style: styles.inputBox}), React.createElement(TouchableHighlight, {onPress: this.onLogin}, React.createElement(Text, null, "Login"))));
+        return (React.createElement(View, {style: styles.container}, React.createElement(Text, {style: styles.title}, "react-native-u2f"), React.createElement(Text, {style: styles.label}, "Username"), React.createElement(TextInput, {returnKeyType: 'next', style: styles.inputBox, autoFocus: true, onChangeText: value => this.setState({ email: value })}), React.createElement(Text, {style: styles.label}, "Password"), React.createElement(TextInput, {secureTextEntry: true, style: styles.inputBox, onChangeText: value => this.setState({ password: value })}), React.createElement(TouchableHighlight, {activeOpacity: 1, underlayColor: "rgb(210, 230, 255)", style: [styles.row, styles.block], onPress: () => this.onLogin()}, React.createElement(Text, {style: styles.button}, "Login"))));
     }
 }
 //# sourceMappingURL=login.js.map
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import * as axios from 'axios';
 import { Component } from 'react';
 import {
+    TouchableNativeFeedback,
     TouchableHighlight,
+    TouchableOpacity,
     StyleSheet,
     StyleSheetProperties,
     Text,
@@ -10,6 +13,12 @@ import {
 } from 'react-native';
 
 const styles: any = StyleSheet.create({
+    wrapper: {
+        borderRadius: 8,
+    },
+    button: {
+        color: '#007AFF',
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -22,7 +31,7 @@ const styles: any = StyleSheet.create({
         margin: 5,
     },
     label: {
-        fontSize: 16,
+        fontSize: 17,
         textAlign: 'center',
         margin: 3,
     },
@@ -33,9 +42,27 @@ const styles: any = StyleSheet.create({
         height: 50,
         textAlign: 'center',
     },
+    row: {
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    block: {
+        padding: 10,
+    },
+    nativeFeedbackButton: {
+        textAlign: 'center',
+        margin: 10,
+    },
 });
 
 
+const API = 'https://app.auth2factor.com/api'
+
+class LoginReq {
+    email: string;
+    password: string;
+    doRequestOTC: boolean;
+}
 
 export interface LoginProps {
 }
@@ -43,10 +70,21 @@ export interface LoginProps {
 export class LoginComponent extends React.Component<LoginProps, {}> {
     constructor(props: LoginProps) {
         super(props);
+        this.state = new LoginReq ();
     }
 
     onLogin() {
+        console.log(this.state);
+    }
 
+    login(param: LoginReq) {
+        return axios.post('/v2/users/authenticate', param)
+            .then((response) => {
+                let model = response.data
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
@@ -58,18 +96,25 @@ export class LoginComponent extends React.Component<LoginProps, {}> {
                 <Text style={styles.label}>
                     Username
                 </Text>
-                <TextInput style={styles.inputBox}>
+                <TextInput returnKeyType={'next'} style={styles.inputBox} autoFocus={true} 
+                onChangeText={ value => this.setState({ email: value })}>
                 </TextInput>
                 <Text style={styles.label}>
                     Password
                 </Text>
-                <TextInput secureTextEntry={true} style={styles.inputBox}>
+                <TextInput secureTextEntry={true} style={styles.inputBox}
+                onChangeText={ value => this.setState({ password: value })}>
                 </TextInput>
-                <TouchableHighlight onPress={this.onLogin}>
-                    <Text>
+                <TouchableHighlight
+                    activeOpacity={1}
+                    underlayColor="rgb(210, 230, 255)"
+                    style={[styles.row, styles.block]}                    
+                    onPress={ () => this.onLogin() }>
+                    <Text style={styles.button}>
                         Login
                     </Text>
                 </TouchableHighlight>
+
             </View>
         );
     }
